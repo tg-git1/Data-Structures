fbfa6b706bc3190225843c53a44cd16d061d32d2
tg-git1 <gupta.tiya@gmail.com>
2025-12-02 17:42:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,15 +17,13 @@ def lesson():
         return jsonify({
             "title": "Stack",
             "definition": "A data structure where the last item added is the first one removed. (LIFO)",
-            "analogy": "A stack of books, all placed one above the other. You first remove the last book you placed",
-            "operations": ["push", "pop"],
+            "analogy": "A stack of books, all placed one above the other. You remove the last book you placed",
             "data": stack
         })
     if topic == "queue":
         return jsonify({
             "title": "Queue",
-            "definition": "A Queue is a data structure where the first item added is the first one removed. (FIFO)",
+            "definition": "A data structure where the first item added is the first one removed. (FIFO)",
             "analogy": "A queue at a ticket counter — the first person in line is served first.",
-            "operations": ["enqueue", "dequeue"],
             "data": queue
         })
