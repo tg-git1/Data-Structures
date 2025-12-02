d726c7fa837c3537d08d6e5f6872d325a5c02edb
tg-git1 <gupta.tiya@gmail.com>
2025-12-02 17:31:24 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -6,19 +6,20 @@ function lesson(topic) {
     })
     .then(res => res.json())
     .then(data => {
-        let html = `
-            <h2>${data.title}</h2>
+        let html = `<h2>${data.title}</h2>
             <p><b>What is it?</b> ${data.definition}</p>
             <p><b>Analogy:</b> ${data.analogy}</p>
             <hr>
             <h3>Interactive Console:</h3>
-            <div>
-        `;
+            <div>`;
 
         if (data.title === "Stack") {
-            html += `
-                <input id="stack-ele" placeholder="Enter element">
+            html += `<input id="stack-ele" placeholder="Enter element">
                 <button onclick="push()">PUSH</button>
-                <button onclick="pop()">POP</button>
-            `;
+                <button onclick="pop()">POP</button>`;
+        }
+        else {
+            html += `<input id="queue-ele" placeholder="Enter value">
+                <button onclick="enqueue()">ENQUEUE</button>
+                <button onclick="dequeue()">DEQUEUE</button>`;
         }
